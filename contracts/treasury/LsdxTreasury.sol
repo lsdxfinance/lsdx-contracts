3793d4fdeefda5032398da2135f79eab82d0c697
cypfisher <crypto.fisher@outlook.com>
2023-03-25 11:36:49 +0800

    Update LsdxTreasury to support multiple reward tokens

--- contracts/treasury/LsdxTreasury.sol
+++ contracts/treasury/LsdxTreasury.sol
@@ -1,33 +1,29 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.9;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
-
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-import "./interfaces/IStakingPool.sol";
-
-contract LsdxTreasury is IStakingPool, Ownable, ReentrancyGuard {
+contract LsdxTreasury is Ownable, ReentrancyGuard {
   using SafeMath for uint256;
   using SafeERC20 for IERC20;
 
   /* ========== STATE VARIABLES ========== */
 
-  IERC20 public rewardsToken;
+  mapping(address => IERC20) public rewardsTokens;
+  address[] public rewardTokensList;
   IERC20 public stakingToken;
   uint256 public periodFinish = 0;
-  uint256 public rewardRate = 0;
-  uint256 public rewardsDuration;
+  mapping(address => uint256) public rewardRates;
+  mapping(address => uint256) public rewardsDurations;
   uint256 public lastUpdateTime;
-  uint256 public rewardPerTokenStored;
+  mapping(address => uint256) public rewardPerTokenStored;
 
-  mapping(address => uint256) public userRewardPerTokenPaid;
-  mapping(address => uint256) public rewards;
+  mapping(address => mapping(address => uint256)) public userRewardPerTokenPaid;
+  mapping(address => mapping(address => uint256)) public rewards;
 
   uint256 internal _totalSupply;
   mapping(address => uint256) private _balances;
@@ -35,16 +31,15 @@ contract LsdxTreasury is IStakingPool, Ownable, ReentrancyGuard {
   /* ========== CONSTRUCTOR ========== */
 
   constructor(
-    address _rewardsToken,
-    address _stakingToken,
-    uint256 _durationInDays
+    address[] memory _rewardsTokens,
+    address _stakingToken
   ) Ownable() {
-    rewardsToken = IERC20(_rewardsToken);
+    for (uint256 i = 0; i < _rewardsTokens.length; i++) {
+      rewardsTokens[_rewardsTokens[i]] = IERC20(_rewardsTokens[i]);
+      rewardTokensList.push(_rewardsTokens[i]);
+    }
     stakingToken = IERC20(_stakingToken);
-    rewardsDuration = _durationInDays.mul(3600 * 24);
   }
-
-  receive() external payable virtual {}
 
   /* ========== VIEWS ========== */
 
@@ -60,57 +55,66 @@ contract LsdxTreasury is IStakingPool, Ownable, ReentrancyGuard {
     return Math.min(block.timestamp, periodFinish);
   }
 
-  function rewardPerToken() public view returns (uint256) {
+  function rewardPerToken(address rewardToken) public view returns (uint256) {
     if (_totalSupply == 0) {
-      return rewardPerTokenStored;
+      return rewardPerTokenStored[rewardToken];
     }
     return
-      rewardPerTokenStored.add(
-        lastTimeRewardApplicable().sub(lastUpdateTime).mul(rewardRate).mul(1e18).div(_totalSupply)
+      rewardPerTokenStored[rewardToken].add(
+        lastTimeRewardApplicable()
+          .sub(lastUpdateTime)
+          .mul(rewardRates[rewardToken])
+          .mul(1e18)
+        .div(_totalSupply)
       );
   }
 
-  function earned(address account) public view returns (uint256) {
-    return _balances[account].mul(rewardPerToken().sub(userRewardPerTokenPaid[account])).div(1e18).add(rewards[account]);
+  function earned(address account, address rewardToken) public view returns (uint256) {
+    require(isSupportedRewardToken(rewardToken), "Reward token not supported");
+    return
+      _balances[account]
+        .mul(rewardPerToken(rewardToken).sub(userRewardPerTokenPaid[account][rewardToken]))
+        .div(1e18)
+        .add(rewards[account][rewardToken]);
   }
 
-  function getRewardForDuration() external view returns (uint256) {
-    return rewardRate.mul(rewardsDuration);
+  function isSupportedRewardToken(address rewardToken) public view returns (bool) {
+    for (uint256 i = 0; i < rewardTokensList.length; i++) {
+      if (rewardTokensList[i] == rewardToken) {
+        return true;
+      }
+    }
+    return false;
   }
+
 
   /* ========== MUTATIVE FUNCTIONS ========== */
 
-  function stake(uint256 amount) external virtual payable nonReentrant updateReward(msg.sender) {
+  function stake(uint256 amount) external nonReentrant updateReward(msg.sender) {
     require(amount > 0, "Cannot stake 0");
     _totalSupply = _totalSupply.add(amount);
     _balances[msg.sender] = _balances[msg.sender].add(amount);
-    // console.log('stake, msg.sender balance: %s', stakingToken.balanceOf(msg.sender));
-    _transferStakingToken(amount);
+    stakingToken.safeTransferFrom(msg.sender, address(this), amount);
     emit Staked(msg.sender, amount);
   }
 
-  function _transferStakingToken(uint256 amount) internal virtual {
-    stakingToken.safeTransferFrom(msg.sender, address(this), amount);
-  }
-
-  function withdraw(uint256 amount) public virtual nonReentrant updateReward(msg.sender) {
+  function withdraw(uint256 amount) public nonReentrant updateReward(msg.sender) {
     require(amount > 0, "Cannot withdraw 0");
     _totalSupply = _totalSupply.sub(amount);
     _balances[msg.sender] = _balances[msg.sender].sub(amount);
-    _withdrawStakingToken(amount);
+    stakingToken.safeTransfer(msg.sender, amount);
     emit Withdrawn(msg.sender, amount);
   }
 
-  function _withdrawStakingToken(uint256 amount) internal virtual {
-    stakingToken.safeTransfer(msg.sender, amount);
-  }
-
   function getReward() public nonReentrant updateReward(msg.sender) {
-    uint256 reward = rewards[msg.sender];
-    if (reward > 0) {
-      rewards[msg.sender] = 0;
-      rewardsToken.safeTransfer(msg.sender, reward);
-      emit RewardPaid(msg.sender, reward);
+    for (uint256 i = 0; i < rewardTokensList.length; i++) {
+      address currentToken = rewardTokensList[i];
+      uint256 reward = rewards[msg.sender][currentToken];
+      if (reward > 0) {
+        rewards[msg.sender][currentToken] = 0;
+        IERC20(currentToken).safeTransfer(msg.sender, reward);
+        emit RewardPaid(msg.sender, currentToken, reward);
+      }
     }
   }
 
@@ -121,50 +125,50 @@ contract LsdxTreasury is IStakingPool, Ownable, ReentrancyGuard {
 
   /* ========== RESTRICTED FUNCTIONS ========== */
 
-  function addRewards(uint256 rewardsAmount) external onlyOwner {
-    if (rewardsAmount > 0) {
-      rewardsToken.safeTransferFrom(msg.sender, address(this), rewardsAmount);
-      notifyRewardAmount(rewardsAmount);
-    }
+  function addRewards(address rewardToken, uint256 rewardAmount, uint256 durationInDays) external onlyOwner {
+    require(rewardAmount > 0, "Reward amount should be greater than 0");
+    require(isSupportedRewardToken(rewardToken), "Reward token not supported");
+    uint256 rewardsDuration = durationInDays.mul(3600 * 24);
+    IERC20(rewardToken).safeTransferFrom(msg.sender, address(this), rewardAmount);
+    notifyRewardAmount(rewardToken, rewardAmount, rewardsDuration);
   }
 
-  function notifyRewardAmount(uint256 reward) internal virtual onlyOwner updateReward(address(0)) {
+  function notifyRewardAmount(address rewardToken, uint256 reward, uint256 rewardsDuration) internal virtual onlyOwner updateReward(address(0)) {
     if (block.timestamp >= periodFinish) {
-      rewardRate = reward.div(rewardsDuration);
+      rewardRates[rewardToken] = reward.div(rewardsDuration);
     } else {
       uint256 remaining = periodFinish.sub(block.timestamp);
-      uint256 leftover = remaining.mul(rewardRate);
-      rewardRate = reward.add(leftover).div(rewardsDuration);
+      uint256 leftover = remaining.mul(rewardRates[rewardToken]);
+      rewardRates[rewardToken] = reward.add(leftover).div(rewardsDuration);
     }
 
-    // Ensure the provided reward amount is not more than the balance in the contract.
-    // This keeps the reward rate in the right range, preventing overflows due to
-    // very high values of rewardRate in the earned and rewardsPerToken functions;
-    // Reward + leftover must be less than 2^256 / 10^18 to avoid overflow.
-    uint balance = rewardsToken.balanceOf(address(this));
-    require(rewardRate <= balance.div(rewardsDuration), "Provided reward too high");
+    uint balance = IERC20(rewardToken).balanceOf(address(this));
+    require(rewardRates[rewardToken] <= balance.div(rewardsDuration), "Provided reward too high");
 
     lastUpdateTime = block.timestamp;
     periodFinish = block.timestamp.add(rewardsDuration);
-    emit RewardAdded(reward);
+    emit RewardAdded(rewardToken, reward);
   }
 
   /* ========== MODIFIERS ========== */
 
   modifier updateReward(address account) {
-    rewardPerTokenStored = rewardPerToken();
-    lastUpdateTime = lastTimeRewardApplicable();
-    if (account != address(0)) {
-      rewards[account] = earned(account);
-      userRewardPerTokenPaid[account] = rewardPerTokenStored;
+    for (uint256 i = 0; i < rewardTokensList.length; i++) {
+      address currentToken = rewardTokensList[i];
+      rewardPerTokenStored[currentToken] = rewardPerToken(currentToken);
+      lastUpdateTime = lastTimeRewardApplicable();
+      if (account != address(0)) {
+        rewards[account][currentToken] = earned(account, currentToken);
+        userRewardPerTokenPaid[account][currentToken] = rewardPerTokenStored[currentToken];
+      }
     }
     _;
   }
 
   /* ========== EVENTS ========== */
 
-  event RewardAdded(uint256 reward);
+  event RewardAdded(address indexed rewardToken, uint256 reward);
   event Staked(address indexed user, uint256 amount);
   event Withdrawn(address indexed user, uint256 amount);
-  event RewardPaid(address indexed user, uint256 reward);
+  event RewardPaid(address indexed user, address indexed rewardToken, uint256 reward);
 }
